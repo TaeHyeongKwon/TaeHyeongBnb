fa3a1de55a6ba9c46ce7f8b625c5ab41746f42b5
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-12 16:12:15 +0900

    fix/entity에 가입경로 추가로 user.service의 함수 parameter변경

--- src/auth/user.service.ts
+++ src/auth/user.service.ts
@@ -2,8 +2,8 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { DataSource, FindOneOptions, Repository } from 'typeorm';
-import { SignUpDto } from './dto/signup.dto';
 import * as bcrypt from 'bcrypt';
+import { SignUpInterface } from './interface/auth.interface';
 
 @Injectable()
 export class UserService {
@@ -15,13 +15,13 @@ export class UserService {
 
   //이메일 및 닉네임 중복 확인
   async findByFields(
-    options: FindOneOptions<SignUpDto>,
+    options: FindOneOptions<SignUpInterface>,
   ): Promise<User | undefined> {
     return await this.userRepository.findOne(options);
   }
 
   //유저 정보 저장
-  async saveUserInfo(userInfo: SignUpDto): Promise<SignUpDto> {
+  async saveUserInfo(userInfo: SignUpInterface): Promise<User> {
     // 주입한 dataSource객체로 QueryRunner생성
     const queryRunner = this.dataSource.createQueryRunner();
     //QueryRunner로 DB연결
@@ -36,6 +36,7 @@ export class UserService {
       user.email = userInfo.email;
       user.nickname = userInfo.nickname;
       user.password = userInfo.password;
+      user.registration_path = userInfo.registration_path;
 
       const result = await queryRunner.manager.save(user);
       //트랜잭션 성공 후 적용
@@ -55,7 +56,7 @@ export class UserService {
   }
 
   //패스워드 해싱
-  async transformPassword(userDto: SignUpDto): Promise<void> {
+  async transformPassword(userDto: SignUpInterface): Promise<void> {
     userDto.password = await bcrypt.hash(userDto.password, 10);
   }
 
