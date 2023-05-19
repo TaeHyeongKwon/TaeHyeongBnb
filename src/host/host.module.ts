8dd57626438231fe7063c4b995942492354f530b
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-19 19:29:17 +0900

    feat/Host등록 API, SMS 폰번호 인증 및 확인 API 작성

--- src/host/host.module.ts
+++ src/host/host.module.ts
@@ -3,9 +3,17 @@ import { HostService } from './host.service';
 import { HostController } from './host.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Host } from '../entities/host.entity';
+import { AuthModule } from '../auth/auth.module';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Host])],
+  imports: [
+    TypeOrmModule.forFeature([Host]),
+    AuthModule,
+    HttpModule,
+    CacheModule.register(),
+  ],
   controllers: [HostController],
   providers: [HostService],
   exports: [HostService],
