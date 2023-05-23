bf6bfe231f9fb968af9f0bd8cfb88295b973ad62
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-23 14:15:25 +0900

    feat/숙소 삭제 예약을 조회하기 위한 예약모듈 임포트

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { HousesController } from './houses.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { House } from 'src/entities/house.entity';
+import { House } from '../entities/house.entity';
+import { ReservationsModule } from '../reservations/reservations.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([House])],
+  imports: [TypeOrmModule.forFeature([House]), ReservationsModule],
   controllers: [HousesController],
   providers: [HousesService],
   exports: [HousesService],
