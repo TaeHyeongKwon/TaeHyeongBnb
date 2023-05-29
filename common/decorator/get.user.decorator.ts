415343797a0712943246c0ab1c49c6ea9a4b2a57
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-29 15:18:30 +0900

    fix/GetUser데코레이터를 관리자정보에 대해서도 사용할 수 있게 수정

--- common/decorator/get.user.decorator.ts
+++ common/decorator/get.user.decorator.ts
@@ -1,8 +1,9 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { User } from '../../src/entities/user.entity';
+import { Manager } from 'src/entities/manager.entity';
 
 export const GetUser = createParamDecorator(
-  (data, context: ExecutionContext): User => {
+  (data, context: ExecutionContext): User | Manager => {
     const req = context.switchToHttp().getRequest();
     return req.user;
   },
