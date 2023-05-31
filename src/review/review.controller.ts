7567ade52722aca41f64f2b32e9f2bedb016f45e
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-31 21:55:24 +0900

    feat/리뷰 작성, 리뷰 조회 API 작성

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
+import { AccessTokenGuard } from '../auth/jwt/access.guard';
+import { GetUser } from 'common/decorator/get.user.decorator';
+import { User } from 'src/entities/user.entity';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewService.create(createReviewDto);
+  //리뷰 작성하기
+  //리뷰 작성은 예약정보조회 페이지에서 가능
+  @Post('/:id')
+  @UseGuards(AccessTokenGuard)
+  @UsePipes(ValidationPipe)
+  createReview(
+    @Param('id') id: number,
+    @Body() createReviewDto: CreateReviewDto,
+    @GetUser() user: User,
+  ): Promise<{ msg: string }> {
+    const reservationId = id;
+    const userId = user.id;
+    return this.reviewService.createReview(
+      userId,
+      reservationId,
+      createReviewDto,
+    );
   }
 
-  @Get()
-  findAll() {
-    return this.reviewService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewService.update(+id, updateReviewDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewService.remove(+id);
+  //리뷰 조회하기
+  //리뷰 조회는 숙소상세조회 페이지에서 가능, 리뷰조회도 누구든지 가능
+  @Get('/:id')
+  findReviewList(@Param('id') id: number) {
+    const houseId = id;
+    return this.reviewService.findAll(houseId);
   }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from '../entities/review.entity';
+import { Repository } from 'typeorm';
+import { HousesService } from 'src/houses/houses.service';
+import { ReservationsService } from 'src/reservations/reservations.service';
 
 @Injectable()
 export class ReviewService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Review) private reviewRepository: Repository<Review>,
+    private housesService: HousesService,
+    private reservationsService: ReservationsService,
+  ) {}
+  async createReview(
+    userId: number,
+    reservationId: number,
+    createReviewDto: CreateReviewDto,
+  ): Promise<{ msg: string }> {
+    const existReservation = await this.reservationsService.findByFields({
+      where: { id: reservationId },
+    });
+
+    //해당 숙소에 예약했던 정보가 없다면 에러
+    if (!existReservation) throw new BadRequestException('예약 정보 없음');
+
+    const houseInfo = await this.housesService.findHouse(
+      existReservation.houseId,
+    );
+
+    //예약날짜의 체크아웃 시간이 리뷰작성 날짜보다 앞선다면 에러
+    const today = new Date().toISOString().split('T')[0];
+    if (existReservation.check_out >= today)
+      throw new BadRequestException('리뷰는 숙소 체크아웃 이후 작성 가능');
+
+    //해당 예약정보로된 리뷰가 이미 작성되어 있다면 에러
+    const existReview = await this.reviewRepository.findOne({
+      where: { reservationId },
+    });
+    if (existReview) throw new ConflictException('해당 예약의 리뷰가 존재');
+
+    console.log(createReviewDto);
+    const reviewInfo = await this.reviewRepository.create({
+      userId,
+      houseId: houseInfo.id,
+      reservationId,
+      ...createReviewDto,
+    });
+
+    console.log(reviewInfo);
+
+    await this.reviewRepository.save(reviewInfo);
+
+    return { msg: '리뷰 작성 성공' };
   }
 
-  findAll() {
-    return `This action returns all review`;
-  }
+  async findAll(houseId): Promise<Review[]> {
+    //해당 숙소 페이지가 있는지 확인
+    await this.housesService.findHouse(houseId);
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
-  }
-
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+    return await this.reviewRepository.find({ where: { houseId } });
   }
 }
