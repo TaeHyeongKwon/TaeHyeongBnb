2e966de3d353550148596ea4f743e68cd4305516
TaeHyeongKwon <1004kth1313@naver.com>
2023-05-31 21:10:52 +0900

    fix/app모듈 리뷰엔티티 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { Host } from './entities/host.entity';
 import { ManagerModule } from './manager/manager.module';
 import { Manager } from './entities/manager.entity';
 import { ReviewModule } from './review/review.module';
+import { Review } from './entities/review.entity';
 
 @Module({
   imports: [
@@ -25,7 +26,7 @@ import { ReviewModule } from './review/review.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [User, House, Reservation, Host, Manager],
+      entities: [User, House, Reservation, Host, Manager, Review],
       synchronize: true,
     }),
     AuthModule,
