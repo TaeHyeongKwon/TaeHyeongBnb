1dac6e80a7873d71c3898bbbf029092cf69f652d
TaeHyeongKwon <1004kth1313@naver.com>
2023-07-04 19:19:04 +0900

    test/ controller 관리자 로그인 테스트 코그 작성

--- src/manager/manager.controller.spec.ts
+++ src/manager/manager.controller.spec.ts
@@ -2,13 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ManagerController } from './manager.controller';
 import { ManagerService } from './manager.service';
 import { ManagerSignUpDto } from './dto/manager.signup.dto';
+import { Response } from 'express';
+import { ManagerLoginDto } from './dto/manager.login.dto';
 
 describe('ManagerController', () => {
   let managerController: ManagerController;
 
   const mockManagerService = {
     managerSignUp: jest.fn(),
+    managerLogin: jest.fn(),
   };
+
+  const mockResponse = {
+    setHeader: jest.fn(),
+    cookie: jest.fn(),
+    json: jest.fn(),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -39,6 +48,39 @@ describe('ManagerController', () => {
 
       expect(result).toEqual('signup result');
       expect(mockManagerService.managerSignUp).toBeCalledTimes(1);
+    });
+  });
+
+  describe('managerLogin', () => {
+    it('managerLogin success case', async () => {
+      const managerLoginDto: ManagerLoginDto = {
+        email: expect.any(String),
+        password: expect.any(String),
+      };
+
+      mockManagerService.managerLogin.mockResolvedValue({
+        managerAccessToken: 'managerAccess',
+        managerRefreshToken: 'managerRefresh',
+      });
+
+      await managerController.managerLogin(managerLoginDto, mockResponse);
+
+      const cookieOption = {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      };
+
+      expect(mockManagerService.managerLogin).toBeCalledWith(managerLoginDto);
+      expect(mockResponse.setHeader).toBeCalledWith(
+        'Authorizetion',
+        'Bearer managerAccess',
+      );
+      expect(mockResponse.cookie).toBeCalledWith(
+        'refresh',
+        'managerRefresh',
+        cookieOption,
+      );
+      expect(mockResponse.json).toBeCalledWith({ msg: '관리자 로그인' });
     });
   });
 });
