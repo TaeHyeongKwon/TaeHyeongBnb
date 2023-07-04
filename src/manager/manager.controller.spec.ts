c28d8caf9c2a5b62f9da9ba5d7a4556709b2cd9c
TaeHyeongKwon <1004kth1313@naver.com>
2023-07-04 19:07:02 +0900

    test/controller managerSignUp 테스트코드 작성

--- src/manager/manager.controller.spec.ts
+++ src/manager/manager.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ManagerController } from './manager.controller';
 import { ManagerService } from './manager.service';
+import { ManagerSignUpDto } from './dto/manager.signup.dto';
 
 describe('ManagerController', () => {
   let managerController: ManagerController;
 
-  const mockManagerService = {};
+  const mockManagerService = {
+    managerSignUp: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,5 +21,24 @@ describe('ManagerController', () => {
 
   it('should be defined', () => {
     expect(managerController).toBeDefined();
+  });
+
+  describe('managerSignUp', () => {
+    it('managerSignUp success case', async () => {
+      const managerSignUpDto: ManagerSignUpDto = {
+        email: expect.any(String),
+        password: expect.any(String),
+        name: expect.any(String),
+        department: expect.any(String),
+        position: expect.any(String),
+      };
+
+      mockManagerService.managerSignUp.mockResolvedValue('signup result');
+
+      const result = await managerController.managerSignUp(managerSignUpDto);
+
+      expect(result).toEqual('signup result');
+      expect(mockManagerService.managerSignUp).toBeCalledTimes(1);
+    });
   });
 });
