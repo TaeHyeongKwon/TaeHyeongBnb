676e553ad2810c86bad820e066376db19700d4c2
TaeHyeongKwon <1004kth1313@naver.com>
2023-07-04 17:02:25 +0900

    test/ managerSignInfo 테스트 코드 작성

--- src/manager/manager.service.spec.ts
+++ src/manager/manager.service.spec.ts
@@ -3,11 +3,17 @@ import { ManagerService } from './manager.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Manager } from '../entities/manager.entity';
 import { JwtService } from '@nestjs/jwt';
+import { ManagerSignUpDto } from './dto/manager.signup.dto';
+import { ConflictException } from '@nestjs/common';
 
 describe('ManagerService', () => {
   let managerService: ManagerService;
 
-  const mockManagerRepository = {};
+  const mockManagerRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   const mockJwtService = {};
 
@@ -29,5 +35,60 @@ describe('ManagerService', () => {
 
   it('should be defined', () => {
     expect(managerService).toBeDefined();
+  });
+
+  describe('managerSignUp', () => {
+    it('managerSignUp success case', async () => {
+      const managerSignInfo: ManagerSignUpDto = {
+        email: expect.any(String),
+        password: 'testpass',
+        name: expect.any(String),
+        department: expect.any(String),
+        position: expect.any(String),
+      };
+
+      mockManagerRepository.findOne.mockResolvedValue(undefined);
+
+      jest.spyOn(managerService, 'transformPassword');
+
+      mockManagerRepository.create.mockImplementation((managerSignInfo) => {
+        return { id: expect.any(Number), ...managerSignInfo };
+      });
+
+      mockManagerRepository.save.mockResolvedValue('save result');
+
+      const result = await managerService.managerSignUp(managerSignInfo);
+
+      expect(result).toEqual('save result');
+      expect(mockManagerRepository.findOne).toBeCalledTimes(1);
+      expect(managerService.transformPassword).toBeCalledTimes(1);
+      expect(mockManagerRepository.create).toBeCalledTimes(1);
+      expect(mockManagerRepository.save).toBeCalledTimes(1);
+      expect(mockManagerRepository.save).toBeCalledWith({
+        id: expect.any(Number),
+        ...managerSignInfo,
+      });
+    });
+
+    it('When the same email already exists', async () => {
+      const managerSignInfo: ManagerSignUpDto = {
+        email: 'testeamil@test.com',
+        password: expect.any(String),
+        name: expect.any(String),
+        department: expect.any(String),
+        position: expect.any(String),
+      };
+
+      mockManagerRepository.findOne.mockResolvedValue({
+        email: 'testeamil@test.com',
+      });
+
+      try {
+        await managerService.managerSignUp(managerSignInfo);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ConflictException);
+        expect(e.message).toEqual('중복 이메일');
+      }
+    });
   });
 });
