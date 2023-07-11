0a4ce5d4cc301b93e02072aa55827152a5183033
TaeHyeongKwon <1004kth1313@naver.com>
2023-07-11 19:26:14 +0900

    test/댓글 서비스 테스트 기본코드 작성 및 댓글 조회 테스트 코드작성

--- src/comment/comment.service.spec.ts
+++ src/comment/comment.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentService } from './comment.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Comment } from '../entities/comment.entity';
+import { HousesService } from '../houses/houses.service';
+import { ReviewService } from '../review/review.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CommentService', () => {
-  let service: CommentService;
+  let commentService: CommentService;
+
+  const mockCommentRepository = {
+    findOne: jest.fn(),
+  };
+
+  const mockHousesService = {};
+
+  const mockReviewService = {};
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentService],
+      providers: [
+        CommentService,
+        {
+          provide: getRepositoryToken(Comment),
+          useValue: mockCommentRepository,
+        },
+        { provide: HousesService, useValue: mockHousesService },
+        { provide: ReviewService, useValue: mockReviewService },
+      ],
     }).compile();
 
-    service = module.get<CommentService>(CommentService);
+    commentService = module.get<CommentService>(CommentService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(commentService).toBeDefined();
+  });
+
+  describe('findByFields', () => {
+    const options = expect.any(Object);
+    it('findByFields success case', async () => {
+      mockCommentRepository.findOne.mockResolvedValue('findOne result');
+
+      const result = await commentService.findByFields(options);
+      expect(result).toEqual('findOne result');
+      expect(mockCommentRepository.findOne).toBeCalledTimes(1);
+      expect(mockCommentRepository.findOne).toHaveBeenCalledWith(options);
+    });
+
+    it('If the comment does not exist', async () => {
+      mockCommentRepository.findOne.mockResolvedValue(undefined);
+
+      try {
+        await commentService.findByFields(options);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('없는 댓글');
+      }
+    });
   });
 });
