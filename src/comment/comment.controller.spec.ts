0fe627ce05c017d713f4956aeca91f3a86c743b9
TaeHyeongKwon <1004kth1313@naver.com>
2023-07-12 16:39:21 +0900

    test/댓글 컨트롤러 댓글생성 테스트코드 작성

--- src/comment/comment.controller.spec.ts
+++ src/comment/comment.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
+import { User } from '../entities/user.entity';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 describe('CommentController', () => {
   let commentController: CommentController;
 
-  const mockCommentService = {};
+  const mockCommentService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentController],
       providers: [{ provide: CommentService, useValue: mockCommentService }],
@@ -18,5 +23,27 @@ describe('CommentController', () => {
 
   it('should be defined', () => {
     expect(commentController).toBeDefined();
+  });
+
+  describe('create', () => {
+    const id = expect.any(Number);
+    const user = new User();
+    user.id = expect.any(Number);
+    const createCommentDto: CreateCommentDto = {
+      content: expect.any(String),
+    };
+    it('create success case', async () => {
+      mockCommentService.create.mockResolvedValue('create result');
+
+      const result = await commentController.create(user, createCommentDto, id);
+
+      expect(result).toEqual('create result');
+      expect(mockCommentService.create).toBeCalledTimes(1);
+      expect(mockCommentService.create).toBeCalledWith(
+        user.id,
+        id,
+        createCommentDto,
+      );
+    });
   });
 });
